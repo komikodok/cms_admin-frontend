d890b51deabd5eb0922a62d02a2b7efd5e3c7c35
komikodok <komikodok666@gmail.com>
2025-02-10 21:52:25 +0700

    minor update

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import SideBar from "./components/SideBar"
+
 function App() {
   return (
-    <>
-
-    </>
+    <body className="h-screen overflow-hidden md:flex">
+      <SideBar/>
+    </body>
   )
 }
 
---
+++ src/components/SideBar.jsx
@@ -0,0 +1,9 @@
+const SideBar = () => {
+  return (
+    <>
+        <div className="text-4xl"></div>
+    </>
+  )
+}
+
+export default SideBar
