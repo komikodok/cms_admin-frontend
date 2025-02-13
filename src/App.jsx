46ce37262343dab2e2cec1517e913717a5f95394
komikodok <komikodok666@gmail.com>
2025-02-13 15:56:39 +0700

    minor update

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import SideBar from "./components/SideBar"
+import SideBar from "./components/sidebar/SideBar"
 import Container from "./components/Container"
 import Body from "./components/Body"
 
---
+++ src/components/sidebar/Menu.jsx
@@ -0,0 +1,23 @@
+/* eslint-disable react/prop-types */
+import { useState } from "react"
+
+const DropDownMenu = ({title, items}) => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    return (
+        <div 
+            onMouseOver={() => setIsOpen(true)}
+            onMouseOut={() => setIsOpen(false)}
+            onClick={() => setIsOpen(prev => !prev)}
+            className={`font-bold items-center m-2 cursor-pointer ${isOpen ? "h-auto" : "h-6"} overflow-hidden`}>
+            <span className="p-2">{title}</span>
+            <ol className={`flex flex-col items-center justify-center ${isOpen ? "translate-y-0" : "translate-y-full"} transition-all duration-300`}>
+              {items.map((item, index) => (
+                <li key={index} className="hover:bg-cyan-100 hover:text-slate-950 opacity-60 w-full rounded-xl py-2 px-10">{item}</li>
+              ))}
+            </ol>
+          </div>
+    )
+}
+
+export default DropDownMenu
